5210603e8de5525fb41a88c7755c792ffc3bc1e0
Cindy Cao <scao@caltech.edu>
2020-12-07 22:09:15 -0800

    scripts  wip

---
+++ software/fig2-Copy10.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import scipy.stats
+
+import iqplot
+import bebi103 
+
+import bokeh.io
+import holoviews as hv
+bokeh.io.output_notebook()
+bebi103.hv.set_defaults()
+
